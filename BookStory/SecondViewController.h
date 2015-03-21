8bc098ec138be657849b45dd5d5e76fbf8f12768
Zhigang 张志刚 <zhigangzhangzhigang@ZhigangmatoMacBook-Air.local>
2015-03-21 22:38:43 +0800

    Initial Commit

---
+++ BookStory/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  BookStory
+//
+//  Created by Zhigang 张志刚 on 3/21/15.
+//  Copyright (c) 2015 Zhigang 张志刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
